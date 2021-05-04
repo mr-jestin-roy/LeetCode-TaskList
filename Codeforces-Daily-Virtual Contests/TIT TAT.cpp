a9267d4cd7e5f6af4d6704f124fe1f8a0e8934e4
mr-jestin-roy <roy_jestin@yahoo.com>
2021-05-04 16:55:50 +0530

    Tit tat codeforces

--- Codeforces-Daily-Virtual Contests/TIT TAT.cpp
+++ Codeforces-Daily-Virtual Contests/TIT TAT.cpp
@@ -1,1 +1,29 @@
-TIT TAT.cpp
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int32_t main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, k;
+		cin >> n >> k;
+		int a[n];
+		for (int i = 0; i < n; i++)
+			cin >> a[i];
+		int i = 0;
+		if (a[i] < k) {
+			k -= a[i];
+			a[n - 1] += a[i];
+			a[i] = 0;
+		}
+		else
+		{
+			a[i] -= k;
+			a[n - 1] += k;
+			k = 0;
+		}
+		for (auto i : a)
+			cout << a[i] << " ";
+	}
+}

c477f32ad1cef70d0a38791830bfe33a5295eb20
mr-jestin-roy <roy_jestin@yahoo.com>
2021-05-10 16:47:21 +0530

    Prev commits due to harddisk error

--- Codeforces-Daily-Virtual Contests/Codeforces Round #720 (Div. 2)/B.cpp
+++ Codeforces-Daily-Virtual Contests/Codeforces Round #720 (Div. 2)/B.cpp
@@ -4,8 +4,43 @@ using namespace std;
 #define mod 1e5
 #define int long long int
 
+int gcd(int a, int b)
+{
+	// Everything divides 0
+	if (a == 0)
+		return b;
+	if (b == 0)
+		return a;
 
+	// base case
+	if (a == b)
+		return a;
 
+	// a is greater
+	if (a > b)
+		return gcd(a - b, b);
+	return gcd(a, b - a);
+}
+void solve() {
+	int n;
+	cin >> n;
+	int a[n + 1];
+	int x = 1e9 + 7;
+	int pos = -1;
+	for (int i = 0; i <= n; i++) {
+		cin >> a[i];
+		if (a[i] < x)
+		{
+			x = a[i];
+			pos = i;
+		}
+	}
+	cout << n - 1 << endl;
+	for (int i = 1; i <= n; i++) {
+		if (i == pos)	continue;
+		cout << pos << ' ' << i << ' ' << x << x + abs(pos - i) << "\n";
+	}
+}
 
 
 int32_t main() {
@@ -14,24 +49,10 @@ int32_t main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
-	int t = 1;
+
+	int t;
 	cin >> t;
 	while (t--) {
-		int A, B;
-		cin >> A >> B;
-		if (B == 1) {
-			cout << "NO" << endl;
-			continue;
-		}
-		else
-		{
-			cout << "YES" << endl;
-			if (B != 2) {		//1 is factor of each number
-				cout << A*(B - 1) << " " << A << " " << A*B << endl;
-			}
-			else {
-				cout << A * 9 << " " << A << " " << A * 10 << endl;
-			}
-		}
+		solve();
 	}
 }

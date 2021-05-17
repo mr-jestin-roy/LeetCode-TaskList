cda6da0511c88d8885f24d98577793de2d934c3d
mr-jestin-roy <roy_jestin@yahoo.com>
2021-05-17 21:12:10 +0530

    Codeforces

---
+++ Educational Round 109/B Permutation Sort.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int max = -1;
+    int min = 100;
+    for (auto &x : a)
+    {
+        cin >> x;
+        if (x > max)
+            max = x;
+        if (x < min)
+            min = x;
+    }
+    int isSorted = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i] > a[i + 1])
+        {
+            isSorted = 1;
+            break;
+        }
+    }
+    if (isSorted == 0)
+    {
+        cout << "0" << endl;
+    }
+    else
+    {
+        if (a[0] == min || a[n - 1] == max)
+        {
+            cout << 1 << endl;
+        }
+        else if (a[0] == max && a[n - 1] == min)
+        {
+            cout << 3 << endl;
+        }
+        else
+            cout << 2 << endl;
+    }
+}
+int32_t main()
+{
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}

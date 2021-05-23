592d876ad5bb5ce01e8ab0810bb5b164beffb10c
mr-jestin-roy <roy_jestin@yahoo.com>
2021-05-23 13:34:15 +0530

    Double ended master

--- Codeforces-Daily-Virtual Contests/C. Double-ended Strings.cpp
+++ Codeforces-Daily-Virtual Contests/C. Double-ended Strings.cpp
@@ -6,13 +6,32 @@ using ll = long long;
 #define fast ios_base::sync_with_stdio(false), cin.tie(NULL)
 #define PI 3.14159265358979323846
 #define endl "\n"
-
+#ANOTHER METHOD TO GET LONGEST COMMON SUBSTRING
+// int hi = INT_MIN;
+//         for (int i = 0; i < a.size(); i++)
+//         {
+//             for (int j = 0; j < b.size(); j++)
+//             {
+//                 if (a[i] == b[j])
+//                 {
+//                     int curr = 0;
+//                     int x = i, y = j;
+//                     while (a[x] == b[y] && x < a.size() && y < b.size())
+//                     {
+//                         curr++;
+//                         x++, y++;
+//                     }
+ 
+//                     hi = max(hi, curr);
+//                 }
+//             }
+//         }
 void solve()
 {
     string a, b;
     cin >> a >> b;
-    int m = strlen(a);
-    int n = strlen(b);
+    int m = a.size();
+    int n = b.size();
     int LCS_table[m + 1][n + 1];
     int LCSlength = 0;
 

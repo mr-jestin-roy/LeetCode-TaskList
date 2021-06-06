f463541c52b666b14655bc1bc897894943b453d7
mr-jestin-roy <roy_jestin@yahoo.com>
2021-06-06 23:26:28 +0530

    Stacks& parentheses

--- LeetCode/Stack & Queues/Valid Parentheses.cpp
+++ LeetCode/Stack & Queues/Valid Parentheses.cpp
@@ -1,6 +1,14 @@
-class Solution {
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+class Solution
+{
 public:
-    bool isValid(string s) {
+    bool isValid(string s)
+    {
         stack<char> stk;
         for (auto i : s)
         {

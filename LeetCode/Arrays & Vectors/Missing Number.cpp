2d71e10989555625fbd01832d6ae2ccd37dc240e
mr-jestin-roy <roy_jestin@yahoo.com>
2021-06-10 13:04:53 +0530

    BIT MANIPULATION

--- LeetCode/Arrays & Vectors/Missing Number.cpp
+++ LeetCode/Arrays & Vectors/Missing Number.cpp
@@ -16,3 +16,20 @@ public:
         return sum;
     }
 };
+
+//APPROACH 2 (Bit Manipulate Solution with Explaination)
+// The basic idea is to use XOR operation. We all know that a^b^b =a, which means two xor operations with the same number will eliminate the number and reveal the original number.
+
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int res = nums.size();
+        for (int i = 0; i < nums.size(); i++)
+        {
+            res = res ^ i ^ nums[i];
+        }
+        return res;
+    }
+};

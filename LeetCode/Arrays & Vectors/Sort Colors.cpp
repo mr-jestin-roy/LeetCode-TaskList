da677516fd25772d123fcbf6595c023ec8a41ba2
mr-jestin-roy <roy_jestin@yahoo.com>
2021-06-10 11:20:41 +0530

    Striver's Problem sheet started

---
+++ LeetCode/Arrays & Vectors/Sort Colors.cpp
@@ -0,0 +1,32 @@
+// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+// You must solve this problem without using the library's sort function.
+
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+        int temp;
+        while (mid <= high)
+        {
+            if (nums[mid] == 0)
+            {
+                swap(nums[low++], nums[mid++]);
+            }
+            else if (nums[mid] == 1)
+            {
+                mid++;
+            }
+            else if (nums[mid] == 2)
+            {
+                swap(nums[mid], nums[high--]);
+            }
+        }
+    }
+};

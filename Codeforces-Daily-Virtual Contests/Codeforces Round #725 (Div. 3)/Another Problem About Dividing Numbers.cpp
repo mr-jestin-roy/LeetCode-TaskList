5d2cf9109189c02a69f1a524affb65b8d6f84dc8
mr-jestin-roy <roy_jestin@yahoo.com>
2021-06-14 21:13:03 +0530

    faster code

--- Codeforces-Daily-Virtual Contests/Codeforces Round #725 (Div. 3)/Another Problem About Dividing Numbers.cpp
+++ Codeforces-Daily-Virtual Contests/Codeforces Round #725 (Div. 3)/Another Problem About Dividing Numbers.cpp
@@ -1,10 +1,9 @@
 // Created by Jestin Roy
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 #define mod 1000000007
-#define ll long long
+// #define ll long long
 #define pb push_back
 #define all(v) v.begin(), v.end()
 #define sz(a) (ll) a.size()
@@ -12,6 +11,68 @@ using ll = long long;
 #define fast ios_base::sync_with_stdio(false), cin.tie(NULL)
 #define PI 3.14159265358979323846
 #define endl "\n"
+template <typename T, typename U>
+static inline void amin(T &x, U y)
+{
+    if (y < x)
+        x = y;
+}
+template <typename T, typename U>
+static inline void amax(T &x, U y)
+{
+    if (x < y)
+        x = y;
+}
+
+void solve()
+{
+    int a, b, k;
+    cin >> a >> b >> k;
+    if (a > b)
+        swap(a, b);
+    if (k == 1)
+    {
+        if (b % a == 0 && (b != a))
+        {
+            cout << "YES" << endl;
+        }
+        else
+            cout << "NO" << endl;
+    }
+    else
+    {
+        int cnt1 = 0;
+        for (int i = 2; i * i <= a; i++) //count factors of a from 2 to sqrt(a)
+        {
+            while (a % i == 0)
+            {
+                cnt1++;
+                a /= i;
+            }
+        }
+        if (a > 1)
+            cnt1++;
+
+        int cnt2 = 0;
+        for (int i = 2; i * i <= b; i++) //count factors of b from 2 to sqrt(b)
+        {
+            while (b % i == 0)
+            {
+                cnt2++;
+                b /= i;
+            }
+        }
+        if (b > 1)
+            cnt2++;
+
+        if (cnt1 + cnt2 >= k)
+        {
+            cout << "YES" << endl;
+        }
+        else
+            cout << "NO" << endl;
+    }
+}
 
 int32_t main()
 {
@@ -24,51 +85,6 @@ int32_t main()
     cin >> t;
     while (t--)
     {
-        int a, b, k;
-        cin >> a >> b >> k;
-        if (a > b)
-            swap(a, b);
-        if (k == 1)
-        {
-            if (b % a == 0 && (b != a))
-            {
-                cout << "YES" << endl;
-            }
-            else
-                cout << "NO" << endl;
-        }
-        else
-        {
-            int cnt1 = 0;
-            for (int i = 2; i * i <= a; i++) //count factors of a from 2 to sqrt(a)
-            {
-                while (a % i == 0)
-                {
-                    cnt1++;
-                    a /= i;
-                }
-            }
-            if (a > 1)
-                cnt1++;
-
-            int cnt2 = 0;
-            for (int i = 2; i * i <= b; i++) //count factors of b from 2 to sqrt(b)
-            {
-                while (b % i == 0)
-                {
-                    cnt2++;
-                    b /= i;
-                }
-            }
-            if (b > 1)
-                cnt2++;
-
-            if (cnt1 + cnt2 >= k)
-            {
-                cout << "YES" << endl;
-            }
-            else
-                cout << "NO" << endl;
-        }
+        solve();
     }
 }

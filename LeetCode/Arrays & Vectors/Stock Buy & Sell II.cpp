b3a80bd73d07d5a8054b6d3bfcd9f81c65e938ce
mr-jestin-roy <roy_jestin@yahoo.com>
2021-06-15 20:37:07 +0530

    check on second method

--- LeetCode/Arrays & Vectors/Stock Buy & Sell II.cpp
+++ LeetCode/Arrays & Vectors/Stock Buy & Sell II.cpp
@@ -31,3 +31,20 @@ public:
         return MaxProfit;
     }
 };
+
+//sum of sub profit = MaxProfit
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int MaxProfit = 0;
+        for (int i = 1; i < prices.size(); i++)
+        {
+            if (prices[i] > prices[i - 1])
+                MaxProfit += prices[i] - prices[i - 1];
+        }
+
+        return MaxProfit;
+    }
+};

1c1c71c75ef00440eeeba3b9db99ca1890b8da39
mr-jestin-roy <roy_jestin@yahoo.com>
2021-06-26 11:29:16 +0530

    pleasant pairs

--- Codeforces-Daily-Virtual Contests/Codeforces Round #726 (Div 2)/Pleasant Pairs.cpp
+++ Codeforces-Daily-Virtual Contests/Codeforces Round #726 (Div 2)/Pleasant Pairs.cpp
@@ -1,50 +1,52 @@
+// // Created by Jestin Roy
+// HINTS: (i+j) <= 2*n
+//      : The number of pairs (a,b) such that a⋅b≤x is O(xlogx)
+
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
-vector<int> factors[200005];
-int32_t main()
+using ll = long long;
+
+#define mod 1000000007
+#define int long long
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL)
+#define PI 3.14159265358979323846
+#define endl "\n"
+
+void solve()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    for (int i = 1; i < 200005; i++)
+    int n;
+    cin >> n;
+    vector<int> v(n + 1, 0);
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = i; j < 200005; j += i)
+        cin >> v[i];
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = v[i]; j <= 2 * n; j += v[i])
         {
-            factors[j].push_back(i);
+            if (j - i > i && (j - i) <= n && v[j - i] == j / v[i])
+            {
+                ans++;
+            }
         }
     }
-    int t;
+
+    cout << ans << endl;
+}
+int32_t main()
+{
+    fast;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
     cin >> t;
     while (t--)
     {
-        int n;
-        cin >> n;
-        int a[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-        }
-        map<int, int> rev;
-        for (int i = 0; i < n; i++)
-        {
-            rev[a[i]] = i + 1;
-        }
-        int ans = 0;
-        for (int i = 2; i <= 2 * n; i++)
-        {
-            for (auto x : factors[i])
-            {
-                //if(x*x>i){continue;}
-                if (rev.find(x) == rev.end() || rev.find(i / x) == rev.end())
-                {
-                    continue;
-                }
-                if (rev[x] + rev[i / x] == i && rev[x] < rev[i / x])
-                {
-                    ans++;
-                }
-            }
-        }
-        cout << ans << "\n";
+        solve();
     }
 }

8a1023f32f4930c276d35fdcb01c01a8b64e94ea
mr-jestin-roy <roy_jestin@yahoo.com>
2021-06-28 03:01:52 +0530

    cyclic

---
+++ Codeforces-Daily-Virtual Contests/Codechef June Starters 2021/Cyclic Quadrilateral.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        if ((a + c == 180) && (b + d == 180))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}

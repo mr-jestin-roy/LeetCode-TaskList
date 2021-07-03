11dc74f91121a895949f672996613197880e2f69
mr-jestin-roy <roy_jestin@yahoo.com>
2021-07-03 23:20:27 +0530

    string question

---
+++ LeetCode/String/FizzBuzz.cpp
@@ -0,0 +1,24 @@
+// Easy
+class Solution
+{
+public:
+    vector<string> fizzBuzz(int n)
+    {
+        vector<string> ans;
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                ans.push_back("FizzBuzz");
+            else if (i % 3 == 0)
+                ans.push_back("Fizz");
+            else if (i % 5 == 0)
+                ans.push_back("Buzz");
+            else
+            {
+                string s = to_string(i);
+                ans.push_back(s);
+            }
+        }
+        return ans;
+    }
+};

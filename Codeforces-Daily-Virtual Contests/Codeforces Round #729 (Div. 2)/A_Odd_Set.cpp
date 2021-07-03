8321ac1cb1a6f62b9be46254d67af6a146a1708d
mr-jestin-roy <roy_jestin@yahoo.com>
2021-07-03 21:26:44 +0530

    cph installed

---
+++ Codeforces-Daily-Virtual Contests/Codeforces Round #729 (Div. 2)/A_Odd_Set.cpp
@@ -0,0 +1,49 @@
+// Created by Jestin Roy
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define mod 1000000007
+#define ll long long
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define sz(a) (ll) a.size()
+#define int long long int
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL)
+#define PI 3.14159265358979323846
+#define endl "\n"
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(2 * n);
+    int cnt = 0;
+    for (int i = 0; i < 2 * n; i++)
+    {
+        cin >> a[i];
+        if (a[i] & 1)
+            cnt++;
+    }
+    if (cnt == n)
+    {
+
+        cout << "Yes" << endl;
+    }
+    else
+        cout << "No" << endl;
+}
+int32_t main()
+{
+    fast;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}

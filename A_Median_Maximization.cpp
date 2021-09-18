dd9361b03576f1877c085760ef1cd1ed7c8ad31b
mr-jestin-roy <roy_jestin@yahoo.com>
2021-09-18 20:54:04 +0530

    cf done today

---
+++ A_Median_Maximization.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,sum;
+        cin>>n>>sum;
+        int median = n/2 + 1;
+        cout<< sum/median <<endl;
+
+    }
+}

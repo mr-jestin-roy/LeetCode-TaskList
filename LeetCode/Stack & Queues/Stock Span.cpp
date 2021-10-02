20c357d38da2316b0f0138312bc7a25c4830c363
mr-jestin-roy <roy_jestin@yahoo.com>
2021-10-03 02:57:49 +0530

    stock span

---
+++ LeetCode/Stack & Queues/Stock Span.cpp
@@ -0,0 +1,20 @@
+class StockSpanner {
+public:
+    stack<pair<int,int>> s;
+    int next(int price) {
+        int res =1;         
+        while(!s.empty() && s.top().first <= price) {
+            res += s.top().second;
+            s.pop();        //Pop lower price from the stack and accumulate the count.
+        }
+        s.push({price,res});
+        return res;
+        
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */

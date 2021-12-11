616f235c504c5b44149ffff5f7675c10de391032
mr-jestin-roy <roy_jestin@yahoo.com>
2021-12-11 07:03:07 +0530

    Striver Graph Series

---
+++ LeetCode/Striver's Graph Series/Graph Representation in C++.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<pair<int, int>> adj[n + 1];
+    for (int i = 0; i < m;i++){
+        int u, v, wt;
+        cin >> u >> v >> wt;
+
+        adj[u].push_back({v, wt});
+        adj[v].push_back({u, wt});
+    }
+    return 0;
+}

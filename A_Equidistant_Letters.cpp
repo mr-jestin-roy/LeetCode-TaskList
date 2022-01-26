364fc43134f378bd38e22bd342155e9bb1cfd092
mr-jestin-roy <roy_jestin@yahoo.com>
2022-01-26 23:11:36 +0530

    Practice codeforces

--- A_Equidistant_Letters.cpp
+++ A_Equidistant_Letters.cpp
@@ -6,12 +6,6 @@ using namespace std;
 #define endl "\n"
 #define inf 1e15
 
-int findTheWinner(int n, int k) {
-        int res = 0;
-        for (int i = 1; i <= n; ++i)
-            res = (res + k) % i;
-        return res + 1;
-}
 void solve()
 {
     string s;

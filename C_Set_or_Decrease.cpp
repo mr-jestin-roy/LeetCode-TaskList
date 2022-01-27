8a4f9aa6ebaf1050ad7fddf7653719a58f022b3e
mr-jestin-roy <roy_jestin@yahoo.com>
2022-01-27 13:02:34 +0530

    codeforces practice done

--- C_Set_or_Decrease.cpp
+++ C_Set_or_Decrease.cpp
@@ -6,20 +6,69 @@ using namespace std;
 #define endl "\n"
 #define inf 1e15
 
+int n, k;
+
+bool helper(vector<int> &arr, int m)
+{
+    int l = max(n - m, (int)1);
+    int r = n - 1;
+    int first = arr[0] - (m - (r - l + 1));
+    while(l<=r)
+    {
+        int left = arr[l] - first;
+        int right = (r - l);
+
+        if(right >= left)
+        {
+            first--;
+            l++;
+        }
+        else{
+            break;
+        }
+    }
+
+    int sum = first;
+    for (int i = 1; i < l;i++){
+        sum += arr[i];
+    }
+    for (int j = l; j <= r;j++){
+        sum += first;
+    }
+
+    return (sum <= k);
+}
+
+
+
+
 void solve()
 {
-    int arr[3];
-    cin>>arr[0]>>arr[1]>>arr[2];
-    sort(arr, arr + 3);
-
-    if(arr[2] == (arr[0]+arr[1]) || (arr[0]== arr[1] && arr[2]%2==0) || (arr[1]==arr[2] && arr[0]%2==0)){
-        cout << "YES" << endl;
+    // int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    int sum = 0;
+    for (int i = 0; i < n;i++){
+        cin >> arr[i];
+        sum += arr[i];
     }
-    else{
-        cout << "NO"<< endl;
-        
+    sort(arr.begin(), arr.end());
+    int l = 0, r = arr[0] + n;
+    int ans = 0;
+    //binary search 
+    while(l<r)
+    {
+        int mid = l + (r - l) / 2;
+        if(helper(arr,mid)){
+            r = mid;
+            ans = mid;
+        }
+        else{
+            l = mid +1;
+        }
     }
 
+    cout << ans << endl;
 }
 
 int32_t main()

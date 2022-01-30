18d212cbacd4dbfea1cd9fa6b16d27ccd0a08e87
mr-jestin-roy <roy_jestin@yahoo.com>
2022-01-30 21:48:02 +0530

    Upsolve after the contest

---
+++ Ringroad_houses.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL)
+#define int long long int
+#define mod 998244353
+#define endl "\n"
+#define inf 1e15
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(m);
+    for(auto &i:a)
+        cin >> i;
+    for (int i = 0; i < m;i++){
+        ///Upsolve after the contest , Contest Name: CODEHUNT
+    }
+}
+
+int32_t main()
+{
+    fast;
+    cout << fixed << setprecision(20);
+    int t = 1;
+    // cin >> t;
+    for (int tt = 1; tt <= t; tt++)
+    {
+        // cout << "Case #" << tt << ": ";
+        solve();
+    }
+}

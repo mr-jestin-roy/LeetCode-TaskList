190dbe92493b7d16aae324ea348e7e95aa2cc098
mr-jestin-roy <roy_jestin@yahoo.com>
2022-01-31 20:01:02 +0530

    pre contest update

---
+++ B_Triangles_on_a_Rectangle.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL)
+#define int long long int
+#define mod 998244353
+#define endl "\n"
+#define inf 1e15
+
+
+void solve()
+{
+    int w, h;
+    cin >> w >> h;
+ 
+    int k;
+ 
+    vector<vector<int>> mat(4, vector<int>());
+ 
+    int ans = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> k;
+ 
+        for (int j = 0; j < k; j++)
+        {
+            int x;
+            cin >> x;
+ 
+            mat[i].push_back(x);
+        }
+        sort(mat[i].begin(), mat[i].end());
+    }
+    ans = max(ans, (mat[0][mat[0].size() - 1] - mat[0][0]) * h);
+    ans = max(ans, (mat[1][mat[1].size() - 1] - mat[1][0]) * h);
+    ans = max(ans, (mat[2][mat[2].size() - 1] - mat[2][0]) * w);
+    ans = max(ans, (mat[3][mat[3].size() - 1] - mat[3][0]) * w);
+ 
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    fast;
+    cout << fixed << setprecision(20);
+    int t = 1;
+    // cin >> t;
+    for (int tt = 1; tt <= t; tt++)
+    {
+        // cout << "Case #" << tt << ": ";
+        solve();
+    }
+}
--- Ringroad_houses.cpp
+++ Ringroad_houses.cpp
@@ -1,21 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define fast ios_base::sync_with_stdio(false), cin.tie(NULL)
-#define int long long int
+// #define int long long int
 #define mod 998244353
 #define endl "\n"
 #define inf 1e15
 
 void solve()
 {
-    int n, m;
-    cin >> n >> m;
-    vector<int> a(m);
-    for(auto &i:a)
-        cin >> i;
-    for (int i = 0; i < m;i++){
-        ///Upsolve after the contest , Contest Name: CODEHUNT
+    long long int n,m,sum = 0;
+    cin>>n>>m;
+    long long int a[m];
+    for(long long int i = 0;i<m;i++){cin>>a[i];}
+    sum = a[0]-1;
+    for(long long int i = 1;i<m;i++){
+        if(a[i]>a[i-1]){sum+=a[i]-a[i-1];}
+        else if(a[i]<a[i-1]){sum += a[i]+n-a[i-1];}
     }
+    cout<<sum<<"\n";
 }
 
 int32_t main()

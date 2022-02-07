93797c9d0e12bc99c2af8e44a79538e8abf17081
mr-jestin-roy <roy_jestin@yahoo.com>
2022-02-07 16:10:27 +0530

    Upsolved Fortune Telling

--- B_Fortune_Telling.cpp
+++ B_Fortune_Telling.cpp
@@ -2,38 +2,78 @@
 using namespace std;
 #define fast ios_base::sync_with_stdio(false), cin.tie(NULL)
 #define int long long int
-#define mod 998244353
+#define mod 1000000007
 #define endl "\n"
-#define inf 1e9 + 1
-void solve(){
+#define inf 1e15
+
+void solve()
+{
     int n, x, y;
     cin >> n >> x >> y;
-    vector<int> arr(n);
-    for(auto &i: arr){
-        cin >> i;
+
+    int flag = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        flag += x;
     }
 
-    
-    
-    int a = x, b = x + 3;
-    
-    for(auto i: arr){
-        // if((y - b)%i == 0 || b ^ y == i){
-        //     cout << "Bob" << endl;
-        //     break;
-        // }
-        // if((y - a)%i == 0 || a ^ y == i){
-        //     cout << "Alice" << endl;
-        //     break;
-            
-        // }
-        
-        // a ^= i;
-        // b ^= i;
-        cout << i << " ";
+    flag = flag & 1;
+
+    if (flag)
+    {
+        if (x & 1)
+        {
+            if (y & 1)
+            {
+                cout << "Bob" << endl;
+            }
+            else
+            {
+                cout << "Alice" << endl;
+            }
+        }
+        else
+        {
+            if (y & 1)
+            {
+                cout << "Alice" << endl;
+            }
+            else
+            {
+                cout << "Bob" << endl;
+            }
+        }
     }
-    cout << endl;
+    else
+    {
+        if (x & 1)
+        {
+            if (y & 1)
+            {
+                cout << "Alice" << endl;
+            }
+            else
+            {
+                cout << "Bob" << endl;
+            }
+        }
+        else
+        {
+            if (y & 1)
+            {
+                cout << "Bob" << endl;
+            }
+            else
+            {
+                cout << "Alice" << endl;
+            }
+        }
+    }
 }
+
 int32_t main()
 {
     fast;

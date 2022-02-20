0493ffb37a0f68176f3b9dfc1cd1e62fc02042d2
mr-jestin-roy <roy_jestin@yahoo.com>
2022-02-20 09:18:40 +0530

    Google Kickstart

---
+++ Sample_Problem.cpp
@@ -0,0 +1,46 @@
+#pragma GCC optimize("Ofast")//Comment optimisations for interactive problems (use endl)
+#include "bits/stdc++.h"
+using namespace std;
+#define int long long
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define all(x) (x).begin(), (x).end()
+#define uniq(v) (v).erase(unique(all(v)), (v).end())
+#define sz(x) (int)((x).size())
+#define fr first
+#define sc second
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL)
+#define mod 998244353
+#define endl "\n"
+#define inf 1e15
+
+void solve()
+{
+   int n,m;
+   cin >> n>>m;
+   vector<int> a, b;
+    int sum =0;
+   for(int i=0;i<n;i++){
+       int x;
+       cin>>x;
+       sum += x;
+   }
+   cout<<(int)sum % m<<endl;
+}
+
+int32_t main()
+{
+    fast;
+    cout << fixed << setprecision(20);
+    int t = 1;
+    cin >> t;
+    for (int tt = 1; tt <= t; tt++)
+    {
+        cout << "Case #" << tt << ": ";
+        solve();
+    }
+}

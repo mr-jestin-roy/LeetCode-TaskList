daddff6f3018e3a2fe48d27a3efe923e410da168
mr-jestin-roy <roy_jestin@yahoo.com>
2022-02-21 19:34:01 +0530

    codeforces practice

---
+++ A_Dragons.cpp
@@ -0,0 +1,53 @@
+#pragma GCC optimize("Ofast")//Comment optimisations for interactive problems (use endl)
+#include "bits/stdc++.h"
+using namespace std;
+#define int long long
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define all(x) (x).begin(), (x).end()
+#define uniq(v) (v).erase(unique(all(v)), (v).end())
+#define sz(x) (int)((x).size())
+#define fr first
+#define sc second
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL)
+#define mod 998244353
+#define endl "\n"
+#define inf 1e15
+
+void solve()
+{
+   int s,n;
+   cin>>s>>n;
+   pair<int, int> arr[n];
+   for (int i = 0; i<n;i++){
+       cin >> arr[i].first >> arr[i].second;
+   }
+   sort(arr, arr + n);
+
+   for (int i = 0; i < n;i++){
+       if(arr[i].first >= s){
+           cout << "NO" << endl;
+           return;
+       }
+       s += arr[i].second;
+   }
+
+   cout << "YES" << endl;
+}
+
+int32_t main()
+{
+    fast;
+    cout << fixed << setprecision(20);
+    int t = 1;
+    // cin >> t;
+    for (int tt = 1; tt <= t; tt++)
+    {
+        // cout << "Case #" << tt << ": ";
+        solve();
+    }
+}

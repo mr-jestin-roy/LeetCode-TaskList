b425ee1eff023d2bead0f021848bf9ab18ab10a3
mr-jestin-roy <roy_jestin@yahoo.com>
2022-02-22 14:54:17 +0530

    codeforces update

---
+++ A_New_Year_Candles.cpp
@@ -0,0 +1,44 @@
+#pragma GCC optimize("Ofast")//Comment optimisations for interactive problems (use endl)
+#include "bits/stdc++.h"
+using namespace std;
+#define int long long
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define all(x) (x).begin(), (x).end()
+#define uniq(v) (v).erase(unique(all(v)), (v).end())
+#define sz(x) (int)((x).size())
+#define fr first
+#define sc second
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL)
+#define mod 998244353
+#define endl "\n"
+#define inf 1e15
+
+void solve()
+{
+   int a,b; cin>>a>>b;
+   int ans = 0;
+   while(a >= b){
+       ans += b;//you are adding new candles formed
+       a -= (b - 1);
+   }
+   ans += a;//remaining first old candles
+   cout << ans << endl;
+}
+
+int32_t main()
+{
+    fast;
+    cout << fixed << setprecision(20);
+    int t = 1;
+    // cin >> t;
+    for (int tt = 1; tt <= t; tt++)
+    {
+        // cout << "Case #" << tt << ": ";
+        solve();
+    }
+}

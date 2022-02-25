7357334c22b0cbb710fb4c3d7a84a20f5c42219b
mr-jestin-roy <roy_jestin@yahoo.com>
2022-02-25 23:49:28 +0530

    codeforces UPDATE Exams Over

---
+++ B_Vanya_and_Lanterns.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL)
+#define int long long int
+#define mod 998244353
+#define endl "\n"
+#define inf 1e15
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    double a[n];
+    for (int i = 0; i < n;i++){
+        cin >> a[i];
+    }
+    sort(a, a + n);
+    double ans = max(a[0], k - a[n - 1]);
+    for (int i = 0; i < n - 1;i++){
+
+        ans = max(ans, ((double)(a[i + 1] - a[i])) / 2.0);
+    }
+//
+    cout << fixed << setprecision(10) << ans << endl;
+}
+
+
+int32_t main()
+{
+    fast;
+    cout << fixed << setprecision(20);
+    int t = 1;
+    // cin >> t;
+    for (int tt = 1; tt <= t; tt++)
+    {
+        // cout << "Case #" << tt << ": ";
+        solve();
+    }
+}

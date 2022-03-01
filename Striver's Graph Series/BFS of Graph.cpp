ac8e95609cf3d1cd55d7a0c7ad7cc6e291f3bf7c
mr-jestin-roy <roy_jestin@yahoo.com>
2022-03-01 13:44:13 +0530

    Updates before the Graph Playlist

---
+++ Striver's Graph Series/BFS of Graph.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL)
+#define int long long int
+#define mod 998244353
+#define endl "\n"
+#define inf 1e9 + 1
+
+vector<int>bfsOfGraph(int V,vector<int> adj[]){
+    vector<int> bfs;
+    vector <int>  vis(V + 1, 0);
+}
+
+int32_t main()
+{
+    fast;
+    cout << fixed << setprecision(20);
+    int t = 1;
+    cin >> t;
+    for (int tt = 1; tt <= t; tt++)
+    {
+        // cout << "Case #" << tt << ": ";
+        int n;
+        cin >> n;
+
+        
+    }
+}
+

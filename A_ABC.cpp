11415ca9009e9a18cfd61092eb90dc2428c14092
mr-jestin-roy <roy_jestin@yahoo.com>
2022-03-05 13:54:52 +0530

    zzzz

--- A_ABC.cpp
+++ A_ABC.cpp
@@ -1,3 +1,36 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define fast ios_base::sync_with_stdio(false), cin.tie(NULL)
+// #define int long long int
+// #define mod 998244353
+// #define endl "\n"
+// #define inf 1e15
+
+
+
+// int32_t main()
+// {
+//     fast;
+//     cout << fixed << setprecision(20);
+//     int n;
+//     cin>>n;
+//     vector<int> v(n);
+//     for(auto &i:v) cin>>i;
+//     int ans = 0;
+//     for (int i = 0; i < n;i++){
+//         if(i==0){
+//             ans += v[i] * v[i + 1];
+//         }
+//         else if(i==n-1){
+//             ans += v[i];
+//         }
+//         else{
+//             ans += (max(v[i - 1], v[i + 1]) * v[i] - min(v[i - 1], v[i + 1]));
+//         }
+//     }
+//     cout << ans;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 #define fast ios_base::sync_with_stdio(false), cin.tie(NULL)

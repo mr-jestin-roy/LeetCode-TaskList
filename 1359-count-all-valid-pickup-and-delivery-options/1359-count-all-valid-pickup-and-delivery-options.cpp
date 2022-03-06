d1f958d913c50a7f9166214fdee10903d7618d67
Jestin Roy <42301788+mr-jestin-roy@users.noreply.github.com>
2022-03-06 11:26:15 +0530

    Time: 0 ms (100.00%), Space: 6 MB (53.14%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
-    //by recusrion method
-    int countOrders(int n, long res =1) {
-        return n ? countOrders(n-1,res*(n*2 - 1)*n % long(1e9+7)): res;
+    //iterative method
+    int countOrders(int n) {
+        long res =1;
+        long mod = 1e9+7;
+        for(int i=1;i<=n;++i){
+            res = res* (i*2 -1)*i % mod;
+        }
+        return res;
     }
+    //by recusrion method
+    // int countOrders(int n, long res =1) {
+    //     return n ? countOrders(n-1,res*(n*2 - 1)*n % long(1e9+7)): res;
+    // }
 };

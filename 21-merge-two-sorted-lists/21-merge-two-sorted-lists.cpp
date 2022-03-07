bfe041061c070d3227b207733b722a2406eca07e
Jestin Roy <42301788+mr-jestin-roy@users.noreply.github.com>
2022-03-07 23:21:27 +0530

    Time: 4 ms (94.94%), Space: 14.9 MB (45.55%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,20 +11,34 @@
 class Solution {
 public:
    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-        //iterative 
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
-        if(l1->val > l2->val)   swap(l1,l2);   
-        ListNode* res = l1;
-        while(l1!=NULL && l2 != NULL){
-            ListNode *temp = NULL;
-            while(l1 != NULL && l1->val <= l2->val){
-                temp = l1;
-                l1= l1->next;
-            }
-            temp->next = l2;
-            swap(l1,l2);
-        }
-        return res;
+        if(NULL == l1) return l2;
+    if(NULL == l2) return l1;
+    
+    ListNode* dummy=NULL;    // head of the list to return
+    
+    // find first element (can use dummy node to put this part inside of the loop)
+    if(l1->val < l2->val)       
+        { dummy = l1; l1 = l1->next; }
+    else                        
+        { dummy = l2; l2 = l2->next; }
+    
+    ListNode* p = dummy;     // pointer to form new list
+    
+    // I use && to remove extra IF from the loop
+    while(l1 && l2){
+        if(l1->val < l2->val)   
+            { p->next = l1; l1 = l1->next; }
+        else                    
+            { p->next = l2; l2 = l2->next; }
+        p=p->next;
+    }
+    
+    // add the rest of the tail, done!
+    if(l1 != NULL)  
+        p->next=l1;
+    else    
+        p->next=l2;
+    
+    return dummy;
        }
 };

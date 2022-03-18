34ee90d7094b72ead6870669c8aa1aa95c3579ff
Jestin Roy <42301788+mr-jestin-roy@users.noreply.github.com>
2022-03-18 12:59:54 +0530

    Time: 4 ms (61.69%), Space: 6.9 MB (20.77%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -6,9 +6,11 @@ public:
         vector<bool> visited(256,false);
         
         for(auto ch : s) dict[ch]++;
+         /** the key idea is to keep a monotically increasing sequence **/
         
         for(auto c:s){
             dict[c]--;
+            /** to filter the previously visited elements **/
             if(visited[c]) continue;
             while(c < ans.back() && dict[ans.back()]){
                 visited[ans.back()] = false;

2ca7578edab353ae552f7137e20e8c3746e2a6de
mr-jestin-roy <roy_jestin@yahoo.com>
2022-05-25 23:05:51 +0530

    Codeforces Round #790 (Div. 4)

---
+++ New_Password.cpp
@@ -0,0 +1,198 @@
+// Author: Jestin Roy
+// Handle Name: jestinroy3
+#pragma GCC optimize("O3,unroll-loops")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll gcd(ll a, ll b)
+{
+    if (b > a)
+    {
+        return gcd(b, a);
+    }
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+ll expo(ll a, ll b, ll mod)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b = b >> 1;
+    }
+    return res;
+}
+void extendgcd(ll a, ll b, ll *v)
+{
+    if (b == 0)
+    {
+        v[0] = 1;
+        v[1] = 0;
+        v[2] = a;
+        return;
+    }
+    extendgcd(b, a % b, v);
+    ll x = v[1];
+    v[1] = v[0] - v[1] * (a / b);
+    v[0] = x;
+    return;
+} // pass an arry of size1 3
+ll mminv(ll a, ll b)
+{
+    ll arr[3];
+    extendgcd(a, b, arr);
+    return arr[0];
+} // for non prime b
+ll mminvprime(ll a, ll b) { return expo(a, b - 2, b); }
+bool revsort(ll a, ll b) { return a > b; }
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact)
+{
+    ll val1 = fact[n];
+    ll val2 = ifact[n - r];
+    ll val3 = ifact[r];
+    return (((val1 * val2) % m) * val3) % m;
+}
+void google(int t) { cout << "Case #" << t << ": "; }
+vector<ll> sieve(int n)
+{
+    int *arr = new int[n + 1]();
+    vector<ll> vect;
+    for (int i = 2; i <= n; i++)
+        if (arr[i] == 0)
+        {
+            vect.push_back(i);
+            for (int j = 2 * i; j <= n; j += i)
+                arr[j] = 1;
+        }
+    return vect;
+}
+ll mod_add(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a + b) % m) + m) % m;
+}
+ll mod_mul(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a * b) % m) + m) % m;
+}
+ll mod_sub(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a - b) % m) + m) % m;
+}
+ll mod_div(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (mod_mul(a, mminvprime(b, m), m) + m) % m;
+} // only for prime m
+ll phin(ll n)
+{
+    ll number = n;
+    if (n % 2 == 0)
+    {
+        number /= 2;
+        while (n % 2 == 0)
+            n /= 2;
+    }
+    for (ll i = 3; i <= sqrt(n); i += 2)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            number = (number / i * (i - 1));
+        }
+    }
+    if (n > 1)
+        number = (number / n * (n - 1));
+    return number;
+} // O(sqrt(N))
+ll getRandomNumber(ll l, ll r) { return uniform_int_distribution<ll>(l, r)(rng); }
+/*--------------------------------------------------------------------------------------------------------------------------*/
+void isAllPresent(string str)
+{
+
+    // Regex to check if a string
+    // contains uppercase, lowercase
+    // special character & numeric value
+    const regex pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$");
+
+    // If the string is empty
+    // then print No
+    if (str.empty())
+    {
+        cout << "No" << endl;
+        return;
+    }
+
+    // Print Yes If the string matches
+    // with the Regex
+    if (regex_match(str, pattern))
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+    return;
+}
+void solve()
+{
+    // start solving the problem from here on
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+    isAllPresent(s);
+}
+
+int main()
+{
+    fastio();
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        google(i);
+        solve();
+    }
+}

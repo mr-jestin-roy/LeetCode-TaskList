e76bd508a0cff63ff04a394be55b4cadf26835e3
mr-jestin-roy <roy_jestin@yahoo.com>
2022-05-31 01:07:08 +0530

    upsolved

---
+++ A_Circular_Local_MiniMax.cpp
@@ -0,0 +1,128 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+#pragma GCC target("avx,avx2,fma")
+#include "bits/stdc++.h"
+using namespace std;
+#define int long long
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define all(x) (x).begin(), (x).end()
+#define uniq(v) (v).erase(unique(all(v)), (v).end())
+#define sz(x) (int)((x).size())
+#define fr first
+#define sc second
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define mem1(a) memset(a, -1, sizeof(a))
+#define mem0(a) memset(a, 0, sizeof(a))
+#define ppc __builtin_popcount
+#define ppcll __builtin_popcountll
+
+template <typename T1, typename T2>
+istream &operator>>(istream &in, pair<T1, T2> &a)
+{
+    in >> a.fr >> a.sc;
+    return in;
+}
+template <typename T1, typename T2>
+ostream &operator<<(ostream &out, pair<T1, T2> a)
+{
+    out << a.fr << " " << a.sc;
+    return out;
+}
+template <typename T, typename T1>
+T amax(T &a, T1 b)
+{
+    if (b > a)
+        a = b;
+    return a;
+}
+template <typename T, typename T1>
+T amin(T &a, T1 b)
+{
+    if (b < a)
+        a = b;
+    return a;
+}
+
+const long long INF = 1e18;
+const int32_t M = 1e9 + 7;
+const int32_t MM = 998244353;
+
+const int N = 0;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+    for (auto &i : a)
+    {
+        cin >> i;
+    }
+    sort(all(a));
+    if (n % 2 == 0)
+    { // even
+        for (int i = 0; i < n / 2; i++)
+        {
+            b[(2 * i) % n] = a[i];
+        }
+        for (int i = n / 2; i < n; i++)
+        {
+            b[(2 * i + 1) % n] = a[i];
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            b[(2 * i) % n] = a[i];
+        }
+    }
+    bool check = true;
+    for (int i = 0; i < n; i++)
+    {
+        if (!((b[i] < b[(i - 1 + n) % n] && b[i] < b[(i + 1) % n]) || (b[i] > b[(i - 1 + n) % n] && b[i] > b[(i + 1) % n])))
+        {
+            check = false;
+            break;
+        }
+    }
+    if (check)
+    {
+        cout << "YES" << endl;
+        for (auto i : b)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+        return;
+    }
+    else
+    {
+        cout << "NO" << endl;
+        return;
+    }
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+#ifdef SIEVE
+    sieve();
+#endif
+#ifdef NCR
+    init();
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}

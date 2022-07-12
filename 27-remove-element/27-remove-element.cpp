e39693d0153063cd9c6f70ae004c0f2c43e45193
Jestin Roy <42301788+mr-jestin-roy@users.noreply.github.com>
2022-07-12 16:13:19 +0530

    Time: 7 ms (21.25%), Space: 8.8 MB (35.17%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        nums.erase(remove(nums.begin(), nums.end(), val), nums.end());
-        return nums.size();
+        int left = 0 , right = nums.size() - 1;
+        while(left <= right){
+            if(nums[left] == val){
+                swap(nums[left], nums[right]);
+                right--;
+            }
+            else
+                left++;
+        }
+        return left;
     }
 };

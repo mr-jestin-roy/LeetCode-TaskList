2e8504556941494f45b4d6d7b04f2b1320a85ade
Jestin Roy <42301788+mr-jestin-roy@users.noreply.github.com>
2022-07-12 08:04:20 +0530

    Time: 27 ms (23.63%), Space: 18.4 MB (74.81%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
+    //USING TWO POINTER METHOD O(N) & SPACE O(1)
     int removeDuplicates(vector<int>& nums) {
-       set<int> s;
-        unique(nums.begin(), nums.end()); //erasing all consecutitive duplicate elements 
-        for(auto i: nums)
-            s.insert(i);
-        
-        return s.size(); //returning no. of unique elements
+       int n = nums.size();
+        if(n == 0) return 0;  // base case
+        int left = 0, right = 0;
+        while(right < n){
+            if(nums[left] != nums[right]){
+                left++;
+                nums[left] = nums[right];
+            }
+            right++;
+        }
+        return left+1;
     }
 };
+// STANDARD SOLUTION + Takes extra memory space for set
+//  int removeDuplicates(vector<int>& nums) {
+//        set<int> s;
+//         unique(nums.begin(), nums.end()); //erasing all consecutitive duplicate elements 
+//         for(auto i: nums)
+//             s.insert(i);
+        
+//         return s.size(); //returning no. of unique elements
+//     }

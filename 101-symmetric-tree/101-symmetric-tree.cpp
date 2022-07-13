a53decd7ffd653b5b664094cad4f13a6e5399d47
Jestin Roy <42301788+mr-jestin-roy@users.noreply.github.com>
2022-07-13 12:23:20 +0530

    Time: 17 ms (8.81%), Space: 16.6 MB (19.14%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -11,20 +11,46 @@
  */
 class Solution {
 public:
-    //Recursive Approach
-    bool SymmetricHelper(TreeNode* root1, TreeNode* root2)
-    {
-        if(root1==NULL && root2 == NULL)
+    //ITERATIVE METHOD
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL)
             return true;
         
-        if(root1 && root2 && root1->val == root2->val){
-            return SymmetricHelper(root1->left, root2->right) && SymmetricHelper(root1->right, root2->left);
-        }
+        if(!root->left && !root->right) //TRUE if it is a single tree
+            return true;
         
-        return false;
-    }
-    bool isSymmetric(TreeNode* root) {
-        return SymmetricHelper(root, root);
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(root);
+        TreeNode* leftNode, *rightNode;
+        
+        while(!q.empty()){
+            leftNode = q.front(); q.pop();
+            rightNode = q.front(); q.pop();
+            // if (NULL == leftNode->val && NULL == rightNode->val)
+            //     continue;
+            // if (!leftNode || !rightNode)
+            //     return false;
+            //both nodes exists , but have different values
+            if(leftNode->val != rightNode->val)  
+                return false;
+            
+            if(leftNode->left && rightNode->right){
+                q.push(leftNode->left);
+                q.push(rightNode->right);
+            }
+            else if(leftNode->left || rightNode->right) //if only one child is present alone in the tree
+            {   return false;}
+            
+            
+            if(leftNode->right && rightNode->left){
+                q.push(leftNode->right);
+                q.push(rightNode->left);
+            }
+            else if(leftNode->right || rightNode->left) //if only one child is present alone in the tree
+            {   return false;   }
+        }
+        return true;
     }
 };
 //  //Recursive Approach

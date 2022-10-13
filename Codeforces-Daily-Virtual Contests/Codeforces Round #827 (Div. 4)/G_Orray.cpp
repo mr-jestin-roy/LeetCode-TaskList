86fca487d331e918e71693b4afa587e22efa04c6
mr-jestin-roy <roy_jestin@yahoo.com>
2022-10-13 23:07:36 +0530

    Post Codeforces Round #827 (Div. 4) Commit

---
+++ Codeforces-Daily-Virtual Contests/Codeforces Round #827 (Div. 4)/G_Orray.cpp
@@ -0,0 +1,106 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+#pragma GCC target("avx,avx2,fma")
+#include "bits/stdc++.h"
+using namespace std;
+#define int long long
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define all(x) (x).begin(), (x).end()
+#define uniq(v) (v).erase(unique(all(v)), (v).end())
+#define sz(x) (int)((x).size())
+#define fr first
+#define sc second
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define mem1(a) memset(a, -1, sizeof(a))
+#define mem0(a) memset(a, 0, sizeof(a))
+#define ppc __builtin_popcount
+#define ppcll __builtin_popcountll
+
+template <typename T1, typename T2>
+istream &operator>>(istream &in, pair<T1, T2> &a)
+{
+    in >> a.fr >> a.sc;
+    return in;
+}
+template <typename T1, typename T2>
+ostream &operator<<(ostream &out, pair<T1, T2> a)
+{
+    out << a.fr << " " << a.sc;
+    return out;
+}
+template <typename T, typename T1>
+T amax(T &a, T1 b)
+{
+    if (b > a)
+        a = b;
+    return a;
+}
+template <typename T, typename T1>
+T amin(T &a, T1 b)
+{
+    if (b < a)
+        a = b;
+    return a;
+}
+
+const long long INF = 1e18;
+const int32_t M = 1e9 + 7;
+const int32_t MM = 998244353;
+
+const int N = 0;
+
+void solve()
+{
+    int n, num = 0;
+    cin >> n;
+    vector<pair<int, int>> Q;
+
+    for (int i = 1, x; i <= n; ++i)
+    {
+        cin >> x;
+        Q.push_back({x, x});
+    }
+    sort(Q.begin(), Q.end());
+    for (int i = 1; i <= n; ++i)
+    {
+        cout << Q[Q.size() - 1].second << " ";
+        bool flag = false;
+        for (int j = 0; j < 30; ++j)
+            if ((num >> j) % 2 == 0 && (Q[Q.size() - 1].second >> j) % 2 == 1)
+            {
+                for (int k = 0; k < (int)Q.size(); ++k)
+                {
+                    Q[k].first &= 2147483647 ^ (1 << j);
+                }
+                flag = true;
+            }
+        num |= Q[Q.size() - 1].second;
+        Q.pop_back();
+        if (flag)
+            sort(Q.begin(), Q.end());
+    }
+    cout << endl;
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+#ifdef SIEVE
+    sieve();
+#endif
+#ifdef NCR
+    init();
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
